cb4469b034c95541babf621c1c9c13d253e024a4
VijayVishwakarma000 <76258542+VijayVishwakarma000@users.noreply.github.com>
2021-04-14 17:48:09 +0530

    TOXIC music bot

--- index.js
+++ index.js
@@ -1,127 +1,78 @@
-const Discord = require('discord.js')
-const{execasync} = require('async_hooks')
-const bot = new Discord.Client()
-const ytdl = require('ytdl-core')
-const {YTSearcher}= require('ytsearcher')
-const config = require('./config.json')
-const searcher = new YTSearcher({
-    key:"AIzaSyCuvXGRw6032R-8t9PJIxpmfFcLY1IFGgU",
-    revealed:true
-})
-const prefix = config.prefix
-const token = config.token
+const Discord = require('discord.js'),
+    DisTube = require('distube'),
+    client = new Discord.Client(),
+    config = require('./config.json')
 
-const queue = new Map()
-bot.on('ready', async (msg) => {
-    console.log('iam ready')
+// Create a new DisTube
+const distube = new DisTube(client, { searchSongs: true, emitNewSongOnly: true });
 
-    bot.user.setActivity(" TOXIC",{
-        type:"LISTENING"
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+     client.user.setActivity("$TOXIC",{
+         type:"PLAYING"
+     })
+});
 
-        
-    })
-})
+client.on("message", async (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(config.prefix)) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift();
 
+    if (command == "play")
+        distube.play(message, args.join(" "));
 
-bot.on('message', async msg=>{
-    const prefix = "$"
+    if (["repeat", "loop"].includes(command))
+        distube.setRepeatMode(message, parseInt(args[0]));
 
-    const serverqueue = queue.get(msg.guild.id)
-
-
-
-    const args = msg.content.slice(prefix.length).trim().split(/ +/g)
-    const cmd = args.shift().toLowerCase()
-
-    if(cmd === 'sing'){
-        exec(msg,serverqueue)
+    if (command == "stop") {
+        distube.stop(message);
+        message.channel.send("Stopped the music!");
     }
 
-    switch(cmd){
-        case 'play':
-            exec(msg,serverqueue)
-            break
-            case 'stop':
-                stop(msg,serverqueue)
-                break    
-            case 'skip':
-                skip(msg,serverqueue)
-                break    
-            }
-    async  function exec (msg,server){
-        var vc = msg.member.voice.channel
-        if(!vc){
-            return msg.reply('Please join a Voice channel')
-        }
-        else{
-            let result = await searcher.search(args.join(" "),{type:"video"})
-            msg.channel.send(result.first.url)
-            const songInfo = await ytdl.getInfo(result.first.url)
-            let song = {
-                title:songInfo.videoDetails.title,
-                url:songInfo.videoDetails.video_url
-            }    
-            if(!serverqueue){
-                const queueconst = {
-                    txtchannel : msg.channel,
-                    vChannel:vc,
-                    connect:null,
-                    songs:[],
-                    volume:10,
-                    playing:true    
-                    
+    if (command == "skip")
+        distube.skip(message);
 
-                }
-                queue.set(msg.guild.id,queueconst)
-                queueconst.songs.push(song)
-                try{
-                    let connection = await vc.join()
-                    queueconst.connection  = connection
-                    play(msg.guild,queueconst.songs[0])
-                }    
-                catch(err){
-                    console.log(err);
-                    queue.delete(msg.guild.id)
-                    return msg.channel.send("Unable to join voice chat "+err)
-                }
-            }
-            else{
-                serverqueue.songs.push(song)
-                return msg.channel.send(`the song has been added ${song.url}`)
-            }
-        }    
-    }function play(guild,song){
-        const serverqueue = queue.get(guild.id)
-        if(!song){
-            serverqueue.vChannel.leave()
-            queue.delete(guild.id)
-            return
-        }
-        const dispatch = serverqueue.connection
-        .play(ytdl(song.url))
-        .on('finish',()=>{
-            serverqueue.song.shift()
-            play(guild,serverqueue.songs[0])
-        })
+    if (command == "queue") {
+        let queue = distube.getQueue(message);
+        message.channel.send('Current queue:\n' + queue.songs.map((song, id) =>
+            `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
+        ).slice(0, 10).join("\n"));
     }
-    function stop(msg,serverqueue){
-        if(!msg.member.voice.channel){
-            msg.member.channel.send("Join voice chat first!")
-            serverqueue.songs =[]
-            serverqueue.connection.dispatch.end()
-        }
-        
-        
-    }
-    function skip(msg,serverqueue){
-        if(!msg.member.voice.channel){
-            return msg.channel.send("join channel first")
 
-        }
-        if(!serverqueue){
-            return msg.channel.send('There is nothing to skip')
-            serverqueue.connection.dispatch.end()
-        }
+    if ([`3d`, `bassboost`, `echo`, `karaoke`, `nightcore`, `vaporwave`].includes(command)) {
+        let filter = distube.setFilter(message, command);
+        message.channel.send("Current queue filter: " + (filter || "Off"));
     }
-})
-bot.login(token)
+});
+
+// Queue status template
+// const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+
+// // DisTube event listeners, more in the documentation page
+// distube
+//     .on("playSong", (message, queue, song) => message.channel.send(
+//         `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
+//     ))
+//     .on("addSong", (message, queue, song) => message.channel.send(
+//         `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+//     ))
+//     .on("playList", (message, queue, playlist, song) => message.channel.send(
+//         `Play \`${playlist.name}\` playlist (${playlist.songs.length} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
+//     ))
+//     .on("addList", (message, queue, playlist) => message.channel.send(
+//         `Added \`${playlist.name}\` playlist (${playlist.songs.length} songs) to queue\n${status(queue)}`
+//     ))
+//     // DisTubeOptions.searchSongs = true
+//     .on("searchResult", (message, result) => {
+//         let i = 0;
+//         message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`);
+//     })
+//     // DisTubeOptions.searchSongs = true
+//     .on("searchCancel", (message) => message.channel.send(`Searching canceled`))
+//     .on("error", (message, e) => {
+//         console.error(e)
+//         message.channel.send("An error encountered: " + e);
+//     });
+
+client.login(config.token);
